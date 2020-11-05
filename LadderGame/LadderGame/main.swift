207834d77555a8035208d1d0efaaba872fb18995
okstring <milkoi7801@gmail.com>
2020-11-05 18:22:05 +0900

    사다리 게임 구현 및 출력

--- LadderGame/LadderGame/main.swift
+++ LadderGame/LadderGame/main.swift
@@ -8,8 +8,22 @@
 import Foundation
 
 print("인원수를 입력해주세요")
-while var participant = readLine() {
-    guard Int(participant)! < 2 || Int(participant)! < 8 else {
-        break
+
+func printLadder(participant: String?, height: Int) {
+    let participantNum = Int(participant!)!
+    var resultLadder = Array(repeating: Array(repeating: "|", count: participantNum * 2 - 1), count: height)
+    
+    for rowIndex in 0..<height {
+        for columnIndex in 0..<participantNum * 2 - 1 {
+            if columnIndex % 2 == 1 && Int.random(in: 0...1) == 1 {
+                resultLadder[rowIndex][columnIndex] = "-"
+            } else if columnIndex % 2 == 1 {
+                resultLadder[rowIndex][columnIndex] = " "
+            }
+            print(resultLadder[rowIndex][columnIndex], terminator: "")
+        }
+        print()
     }
 }
+
+printLadder(participant: readLine(), height: 4)
