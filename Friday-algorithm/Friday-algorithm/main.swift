ccbe0846bc3700ef9347a9471a7e8e4321da8b33
okstring <milkoi7801@gmail.com>
2020-11-06 14:51:14 +0900

    11654번 문제 업로드

--- Friday-algorithm/Friday-algorithm/main.swift
+++ Friday-algorithm/Friday-algorithm/main.swift
@@ -7,9 +7,24 @@
 
 import Foundation
 
-//p1000()
-//p1008()
-//p2438()
-//p2920()
-p8958()
-//p11654()
+// 문제 실행
+func run(code: String) {
+    switch code {
+    case "p1000":
+        p1000()
+    case "p1008":
+        p1008()
+    case "p2438":
+        p2438()
+    case "p2920":
+        p2920()
+    case "p8958":
+        p8958()
+    case "p11654":
+        p11654()
+    default:
+        print("꼭 풀어보겠습니다 :)")
+    }
+}
+
+run(code: "p11654")
---
+++ Friday-algorithm/Friday-algorithm/p11654.swift
@@ -0,0 +1,14 @@
+//
+//  p11654.swift
+//  Friday-algorithm
+//
+//  Created by Maylily on 2020/11/06.
+//
+
+import Foundation
+
+func p11654() {
+    let input = readLine()!
+    let convertAscii = Character(input).asciiValue
+    print(convertAscii!)
+}
