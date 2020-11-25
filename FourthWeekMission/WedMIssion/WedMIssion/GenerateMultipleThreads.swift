c7e362d701cbec65bb7365250ea2f6180ec43982
okstring <milkoi7801@gmail.com>
2020-11-25 21:00:01 +0900

    Modify DispatchQueue(unsolved)

--- FourthWeekMission/WedMIssion/WedMIssion/GenerateMultipleThreads.swift
+++ FourthWeekMission/WedMIssion/WedMIssion/GenerateMultipleThreads.swift
@@ -3,7 +3,7 @@ import Foundation
 class GenerateMultipleThreads {
     private var book = ""
     private let findWordsList = ["대중", "경제", "재산", "자유", "국제", "시장","사상", "부자", "학자", "개인", "욕망", "생활", "자원", "사람", "노동", "인물", "소비", "사회", "이론", "새로운"]
-    private var lap: TimeInterval = 0
+    private var lap:TimeInterval = 0
 
     func readBook(by fileName: String) {
         let path = FileManager.default.urls(for: .desktopDirectory, in: .userDomainMask)[0].appendingPathComponent(fileName)
@@ -14,25 +14,30 @@ class GenerateMultipleThreads {
         }
     }
     private func findWords() {
-        lap = 0
         var countWords = Dictionary<String, Int>()
         for word in self.findWordsList {
-            let beforeLap = Date().timeIntervalSince1970
             countWords[word] = self.book.components(separatedBy: word).count - 1
-            let afterLap = Date().timeIntervalSince1970
-            lap += afterLap - beforeLap
         }
         print(countWords)
-        print(lap)
     }
 
     func unusedDispatchQueue() {
+        lap = 0
+        let beforeLap = Date().timeIntervalSince1970
         findWords()
+        let afterLap = Date().timeIntervalSince1970
+        print(afterLap - beforeLap)
     }
 
     func usedDispatchQueue() {
+        lap = 0
+        let beforeLap = Date().timeIntervalSince1970
         DispatchQueue.main.async {
             self.findWords()
+        }
+        DispatchQueue.main.async {
+            let afterLap = Date().timeIntervalSince1970
+            print(afterLap - beforeLap)
         }
     }
 }
